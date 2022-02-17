904b31288400c92e1fd5ec0c5f1dbe207b762f95
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-02-17 11:42:12 -0300

    Register users with username and passwords

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect('mongodb://localhost:27017/userDB');
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model('User', userSchema);
+
 app.get('/', (request, response) => {
     response.redirect('/home');
 });
@@ -24,6 +33,38 @@ app.get('/login', (request, response) => {
 app.get('/register', (request, response) => {
     response.render('register');
 })
+
+app.post('/register', (request, response) => {
+    const newUser = new User({
+        email: request.body.username,
+        password: request.body.password
+    });
+
+    newUser.save((error) => {
+        if (error) {
+            console.log(error);
+        } else {
+            response.render('secrets');
+        }
+    });
+});
+
+app.post('/login', (request, response) => {
+    const username = request.body.username;
+    const password = request.body.password;
+
+    User.findOne({ email: username }, (error, queryResult) => {
+        if (error) {
+            console.log(error);
+        } else {
+            if (queryResult) {
+                if (queryResult.password === password) {
+                    response.render('secrets');
+                }
+            }
+        }
+    });
+});
 
 app.listen(3000, () => {
     console.log('✔ Server is running on port 3000');
